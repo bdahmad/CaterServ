8e3f3f263cc0c7d46b9083bb71b7e7270345492d
adahmadali <ahmadali09787@gmail.com>
2023-10-10 14:22:45 +0600

    admin mastering done

---
+++ dashboard/functions/function.php
@@ -0,0 +1,15 @@
+<?php
+
+   function getHeader(){
+      require_once('includes/header.php');
+   }
+   function getSidebar(){
+      require_once('includes/sidebar.php');
+   }
+   function getNavbar(){
+      require_once('includes/navbar.php');
+   }
+   function getFooter(){
+      require_once('includes/footer.php');
+   }
+
