1d47259f0eb89ca5012c1dae17c0e69e148c66f0
adahmadali <ahmadali09787@gmail.com>
2023-10-19 14:41:32 +0600

    working on event tabs section

--- includes/event.php
+++ includes/event.php
@@ -3,10 +3,9 @@ include_once('class/EventCategory.php');
 include_once('class/Event.php');
 
 $objEventCategory = new EventCategory();
-$query = $objEventCategory->view();
+
 $objEvent = new Event();
 $queryEvent = $objEvent->view();
-$dat = mysqli_fetch_assoc($query)
 
 ?>
 <!-- Events Start -->
@@ -17,51 +16,99 @@ $dat = mysqli_fetch_assoc($query)
             <h1 class="display-5 mb-5">Our Social & Professional Events Gallery</h1>
         </div>
 
-        
+
         <div class="tab-class text-center">
             <ul class="nav nav-pills d-inline-flex justify-content-center mb-5 wow bounceInUp" data-wow-delay="0.1s">
-                <?php while ($data = mysqli_fetch_assoc($query)) { ?>
-                    <li class="nav-item p-2">
-                        <a class="d-flex mx-2 py-2 border border-primary bg-light rounded-pill active" data-bs-toggle="pill" href="#<?= $data['event_cate_slug']; ?>">
-                            <span class="text-dark" style="width: 150px;"><?= $data['event_cate_name']; ?></span>
-                        </a>
-                    </li>
-                <?php } ?>
+                <?php
+                $c = 1;
+                $query = $objEventCategory->view();
+                while ($data = mysqli_fetch_assoc($query)) {
+
+                    if ($c == 1) {
+                        $c++; ?>
+                        <li class="nav-item p-2">
+                            <a class="d-flex mx-2 py-2 border border-primary bg-light rounded-pill active" data-bs-toggle="pill" href="#tab-1">
+                                <span class="text-dark" style="width: 150px;"><?= $data['event_cate_name']; ?></span>
+                            </a>
+                        </li>
+                    <?php
+                    } else {
+                    ?>
+                        <li class="nav-item p-2">
+                            <a class="d-flex mx-2 py-2 border border-primary bg-light rounded-pill" data-bs-toggle="pill" href="#tab-<?= $data['event_cate_slug']; ?>">
+                                <span class="text-dark" style="width: 150px;"><?= $data['event_cate_name']; ?></span>
+                            </a>
+                        </li>
+                <?php
+                    }
+                }
+                ?>
             </ul>
             <div class="tab-content">
                 <?php
-                $queryEvent = $objEvent->view();
-                while ($dataEv = mysqli_fetch_assoc($queryEvent)) {
-                ?>
-                    <div id="<?= $dat['event_cate_slug']; ?>" class="tab-pane fade show p-0 active">
-                        <div class="row g-4">
+                $t = 1;
+                while ($dataEV = mysqli_fetch_assoc($queryEvent)) {
+                    if ($t == 1) {
+                        $t++; ?>
+                        <div id="tab-1" class="tab-pane fade show p-0 active">
                             <div class="col-lg-12">
                                 <div class="row g-4">
                                     <?php
-
-                                    // while ($dataEvent = mysqli_fetch_assoc($queryEvent)) {
-                                        if($dat['event_cate_id']==$dataEv['event_cate_id']){
-                                    ?>
-                                    <div class="col-md-6 col-lg-3 wow bounceInUp" data-wow-delay="0.1s">
-                                        <div class="event-img position-relative">
-                                            <img class="img-fluid rounded w-100" src="dashboard/uploads/<?= $dataEv['event_image']; ?>" alt="">
-                                            <!-- <div class="event-overlay d-flex flex-column p-4">
-                                                    <h4 class="me-auto"><? //= $dataEv['event_cate_name']; 
-                                                                        ?></h4>
-                                                    <a href="dashboard/uploads/<? //= $dataEv['event_image']; 
-                                                                                ?>" data-lightbox="event-1" class="my-auto"><i class="fas fa-search-plus text-dark fa-2x"></i></a>
-                                                </div> -->
+                                    while ($dataEV = mysqli_fetch_assoc($queryEvent)) { ?>
+                                        <div class="col-md-6 col-lg-3 wow bounceInUp" data-wow-delay="0.1s">
+                                            <div class="event-img position-relative">
+                                                <img class="img-fluid rounded w-100" src="dashboard/uploads/<?= $dataEV['event_image']; ?>" alt="">
+                                                <div class="event-overlay d-flex flex-column p-4">
+                                                    <h4 class="me-auto"><?= $dataEV['event_cate_name']; ?></h4>
+                                                    <a href="dashboard/uploads/<?= $dataEV['event_image']; ?>" data-lightbox="event-1" class="my-auto"><i class="fas fa-search-plus text-dark fa-2x"></i></a>
+                                                </div>
+                                            </div>
                                         </div>
-                                    </div>
-                                    <?php } 
-                                    ?>
+
+
+                                    <?php } ?>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                <?php } ?>
+                    <?php
+                    } else {
+                    ?>
+                        <div id="tab-<?= $data['event_cate_slug']; ?>" class="tab-pane fade show p-0">
+                            <!-- <div class="row g-4"> -->
+                                <div class="col-lg-12">
+                                    <div class="row g-4">
+                                    <?php
+                                    while ($dataEV = mysqli_fetch_assoc($queryEvent)) { ?>
+                                        <div class="col-md-6 col-lg-3">
+                                            <div class="event-img position-relative">
+                                                <img class="img-fluid rounded w-100" src="dashboard/uploads/<?= $dataEV['event_image']; ?>" alt="">
+                                                <div class="event-overlay d-flex flex-column p-4">
+                                                    <h4 class="me-auto">Wedding</h4>
+                                                    <a href="dashboard/uploads/<?= $dataEV['event_image']; ?>" data-lightbox="event-8" class="my-auto"><i class="fas fa-search-plus text-dark fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                        <?php } ?>
+                                        <!-- <div class="col-md-6 col-lg-3">
+                                            <div class="event-img position-relative">
+                                                <img class="img-fluid rounded w-100" src="img/event-2.jpg" alt="">
+                                                <div class="event-overlay d-flex flex-column p-4">
+                                                    <h4 class="me-auto">Wedding</h4>
+                                                    <a href="img/01.jpg" data-lightbox="event-9" class="my-auto"><i class="fas fa-search-plus text-dark fa-2x"></i></a>
+                                                </div>
+                                            </div>
+                                        </div> -->
+                                    </div>
+                                </div>
+                            <!-- </div> -->
+                        </div>
+                <?php
+                    }
+                }
+                ?>
             </div>
         </div>
     </div>
+</div>
 </div>
 <!-- Events End -->
--- tables/cater_serv.sql
+++ tables/cater_serv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 10, 2023 at 12:55 PM
+-- Generation Time: Oct 19, 2023 at 05:37 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -20,6 +20,100 @@ SET time_zone = "+00:00";
 --
 -- Database: `cater_serv`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_about`
+--
+
+CREATE TABLE `tbl_about` (
+  `about_id` int(11) NOT NULL,
+  `about_heading` varchar(50) NOT NULL,
+  `about_para` varchar(200) NOT NULL,
+  `about_service_1` varchar(150) NOT NULL,
+  `about_service_2` varchar(150) NOT NULL,
+  `about_service_3` varchar(150) NOT NULL,
+  `about_service_4` varchar(150) NOT NULL,
+  `about_photo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_about`
+--
+
+INSERT INTO `tbl_about` (`about_id`, `about_heading`, `about_para`, `about_service_1`, `about_service_2`, `about_service_3`, `about_service_4`, `about_photo`) VALUES
+(1, 'Trusted By 200 + satisfied clients', 'Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore eit esdioilore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullaemco laboeeiris nis.', 'Fresh and Fast food Delivery', 'Easy Customization Options', '24/7 Customer Support ', 'Delicious Deals for Delicious Meals ', 'about_1697134113_82657214.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_achievement`
+--
+
+CREATE TABLE `tbl_achievement` (
+  `achieve_id` int(11) NOT NULL,
+  `achieve_customers` varchar(10) NOT NULL,
+  `achieve_chefs` varchar(10) NOT NULL,
+  `achieve_events` varchar(10) NOT NULL,
+  `achieve_video` varchar(250) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_achievement`
+--
+
+INSERT INTO `tbl_achievement` (`achieve_id`, `achieve_customers`, `achieve_chefs`, `achieve_events`, `achieve_video`) VALUES
+(1, '5', '522', '120', 'https://www.youtube.com/watch?v=Rmcrrf4p_VA&ab_channel=%E0%A6%AE%E0%A6%BE%E0%A6%B0%E0%A6%95%E0%A6%BE%E0%A6%9C%E0%A6%9F%E0%A6%BF%E0%A6%AD%E0%A6%BFMarkazTv');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_event`
+--
+
+CREATE TABLE `tbl_event` (
+  `event_id` int(11) NOT NULL,
+  `event_cate_id` int(11) NOT NULL,
+  `event_image` varchar(55) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_event`
+--
+
+INSERT INTO `tbl_event` (`event_id`, `event_cate_id`, `event_image`) VALUES
+(3, 2, 'event_1697438168_58235546.jpg'),
+(4, 3, 'event_1697438610_63880840.jpg'),
+(5, 3, 'event_1697438619_19662693.jpg'),
+(6, 4, 'event_1697438628_45202075.jpg'),
+(7, 5, 'event_1697438639_30240226.png'),
+(8, 2, 'event_1697447759_35940627.jpg'),
+(9, 3, 'event_1697447768_80127793.jpg'),
+(10, 4, 'event_1697447777_2911337.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_event_category`
+--
+
+CREATE TABLE `tbl_event_category` (
+  `event_cate_id` int(11) NOT NULL,
+  `event_cate_name` varchar(25) NOT NULL,
+  `event_cate_slug` varchar(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_event_category`
+--
+
+INSERT INTO `tbl_event_category` (`event_cate_id`, `event_cate_name`, `event_cate_slug`) VALUES
+(1, 'All Event', 'all_event'),
+(2, 'Wedding', 'wedding'),
+(3, 'Corporate', 'corporate'),
+(4, 'Cocktail', 'cocktail'),
+(5, 'Buffet', 'buffet');
 
 -- --------------------------------------------------------
 
@@ -46,6 +140,31 @@ INSERT INTO `tbl_role` (`role_id`, `role_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_services`
+--
+
+CREATE TABLE `tbl_services` (
+  `service_id` int(11) NOT NULL,
+  `service_icon` varchar(25) NOT NULL,
+  `service_title` varchar(25) NOT NULL,
+  `service_description` text NOT NULL,
+  `service_link` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_services`
+--
+
+INSERT INTO `tbl_services` (`service_id`, `service_icon`, `service_title`, `service_description`, `service_link`) VALUES
+(1, 'fas fa-calendar', '  this is title  ', 'this is description', 'www.link.com'),
+(3, 'fas fa-bold', 'adfafasd', 'fasdfas', 'fasfasf'),
+(4, 'fab fa-app-store-ios', '  this is title  ', 'this is description', 'this is link'),
+(5, 'fas fa-gem', ' this sis service ', 'service description', 'link'),
+(6, 'fab fa-codepen', 'sdfsfgasdgfasdga', 'adgfasgagag', 'adgagag');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_status`
 --
 
@@ -68,6 +187,31 @@ INSERT INTO `tbl_status` (`status_id`, `status_name`) VALUES
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `tbl_team`
+--
+
+CREATE TABLE `tbl_team` (
+  `team_id` int(11) NOT NULL,
+  `team_name` varchar(25) NOT NULL,
+  `team_designation` varchar(25) NOT NULL,
+  `team_facebook` varchar(50) NOT NULL,
+  `team_twitter` varchar(50) NOT NULL,
+  `team_instagram` varchar(50) NOT NULL,
+  `team_slug` varchar(15) NOT NULL,
+  `team_photo` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_team`
+--
+
+INSERT INTO `tbl_team` (`team_id`, `team_name`, `team_designation`, `team_facebook`, `team_twitter`, `team_instagram`, `team_slug`, `team_photo`) VALUES
+(1, 'Ahmad Ali', 'Head Chef', 'https://www.facebook.com/', 'https://twitter.com/i/flow/', 'https://www.instagram.com/', 'U652984c76719e', 'team_1697219783_3203573.jpg'),
+(2, 'Reja Kazi', 'Decoration Chef', 'https://www.facebook.com/', 'twitter.com', 'instagram.com', 'U6529871c0979c', 'team_1697220380_35454765.jpg');
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `tbl_users`
 --
 
@@ -75,7 +219,7 @@ CREATE TABLE `tbl_users` (
   `user_id` int(11) NOT NULL,
   `user_name` varchar(25) NOT NULL,
   `user_email` varchar(30) NOT NULL,
-  `user_phone` int(15) NOT NULL,
+  `user_phone` varchar(15) NOT NULL,
   `user_username` varchar(20) NOT NULL,
   `user_password` varchar(35) NOT NULL,
   `user_designation` varchar(20) NOT NULL,
@@ -87,8 +231,43 @@ CREATE TABLE `tbl_users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
+-- Dumping data for table `tbl_users`
+--
+
+INSERT INTO `tbl_users` (`user_id`, `user_name`, `user_email`, `user_phone`, `user_username`, `user_password`, `user_designation`, `user_address`, `user_slug`, `role_id`, `status_id`, `user_photo`) VALUES
+(2, 'Ahmad', 'ahmad@gmail.com', '1935056526', 'ahmad', '6512bd43d9caa6e02c990b0a82652dca', 'engg', 'mirpur dhaka', 'jfasdjlkf', 1, 1, 'users_1697088015_25761476.jpg'),
+(3, 'ahmad', 'ahmad@gmail.com', '1234569877', 'ahmad', '6512bd43d9caa6e02c990b0a82652dca', 'mirpur data', 'dfjalfjdalk', 'U65266773376cd', 1, 5, 'users_1697087424_51258287.jpg'),
+(4, 'Sabbir', 'sabbir@gmail.com', '1236978541', 'sabbir', '6512bd43d9caa6e02c990b0a82652dca', 'engg', 'manik', 'U6526680b4500d', 4, 3, 'users_1697087379_63971349.jpg'),
+(5, 'Ashfuq Ahmed', 'ashfuq@gmail.com', '1234987456', 'aadf', 'c4ca4238a0b923820dcc509a6f75849b', 'Graphic Designer', 'Banai', 'U65267dea4a89a', 4, 5, 'users_1697087287_13908286.jpg');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `tbl_about`
+--
+ALTER TABLE `tbl_about`
+  ADD PRIMARY KEY (`about_id`);
+
+--
+-- Indexes for table `tbl_achievement`
+--
+ALTER TABLE `tbl_achievement`
+  ADD PRIMARY KEY (`achieve_id`);
+
+--
+-- Indexes for table `tbl_event`
+--
+ALTER TABLE `tbl_event`
+  ADD PRIMARY KEY (`event_id`),
+  ADD KEY `event_cate_id` (`event_cate_id`);
+
+--
+-- Indexes for table `tbl_event_category`
+--
+ALTER TABLE `tbl_event_category`
+  ADD PRIMARY KEY (`event_cate_id`);
 
 --
 -- Indexes for table `tbl_role`
@@ -97,10 +276,22 @@ ALTER TABLE `tbl_role`
   ADD PRIMARY KEY (`role_id`);
 
 --
+-- Indexes for table `tbl_services`
+--
+ALTER TABLE `tbl_services`
+  ADD PRIMARY KEY (`service_id`);
+
+--
 -- Indexes for table `tbl_status`
 --
 ALTER TABLE `tbl_status`
   ADD PRIMARY KEY (`status_id`);
+
+--
+-- Indexes for table `tbl_team`
+--
+ALTER TABLE `tbl_team`
+  ADD PRIMARY KEY (`team_id`);
 
 --
 -- Indexes for table `tbl_users`
@@ -115,10 +306,40 @@ ALTER TABLE `tbl_users`
 --
 
 --
+-- AUTO_INCREMENT for table `tbl_about`
+--
+ALTER TABLE `tbl_about`
+  MODIFY `about_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_achievement`
+--
+ALTER TABLE `tbl_achievement`
+  MODIFY `achieve_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `tbl_event`
+--
+ALTER TABLE `tbl_event`
+  MODIFY `event_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
+
+--
+-- AUTO_INCREMENT for table `tbl_event_category`
+--
+ALTER TABLE `tbl_event_category`
+  MODIFY `event_cate_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `tbl_role`
 --
 ALTER TABLE `tbl_role`
   MODIFY `role_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tbl_services`
+--
+ALTER TABLE `tbl_services`
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `tbl_status`
@@ -127,14 +348,26 @@ ALTER TABLE `tbl_status`
   MODIFY `status_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
+-- AUTO_INCREMENT for table `tbl_team`
+--
+ALTER TABLE `tbl_team`
+  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
 -- AUTO_INCREMENT for table `tbl_users`
 --
 ALTER TABLE `tbl_users`
-  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `tbl_event`
+--
+ALTER TABLE `tbl_event`
+  ADD CONSTRAINT `tbl_event_ibfk_1` FOREIGN KEY (`event_cate_id`) REFERENCES `tbl_event_category` (`event_cate_id`);
 
 --
 -- Constraints for table `tbl_users`
