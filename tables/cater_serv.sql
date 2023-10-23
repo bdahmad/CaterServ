eb79fcd99270f06c69c7a786eef06bc75297b5c1
adahmadali <ahmadali09787@gmail.com>
2023-10-23 17:00:27 +0600

    add table

--- tables/cater_serv.sql
+++ tables/cater_serv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 19, 2023 at 05:37 AM
+-- Generation Time: Oct 23, 2023 at 12:59 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -43,7 +43,7 @@ CREATE TABLE `tbl_about` (
 --
 
 INSERT INTO `tbl_about` (`about_id`, `about_heading`, `about_para`, `about_service_1`, `about_service_2`, `about_service_3`, `about_service_4`, `about_photo`) VALUES
-(1, 'Trusted By 200 + satisfied clients', 'Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore eit esdioilore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullaemco laboeeiris nis.', 'Fresh and Fast food Delivery', 'Easy Customization Options', '24/7 Customer Support ', 'Delicious Deals for Delicious Meals ', 'about_1697134113_82657214.jpg');
+(1, 'Trusted By 200 + satisfied clients', 'Consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore eit esdioilore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullaemco laboeeiris nis.', 'Fresh and Fast food Delivery', 'Easy Customization Options', '24/7 Customer Support ', 'Delicious Deals for Delicious Meals ', 'about_1698048743_59323014.jpg');
 
 -- --------------------------------------------------------
 
@@ -64,7 +64,73 @@ CREATE TABLE `tbl_achievement` (
 --
 
 INSERT INTO `tbl_achievement` (`achieve_id`, `achieve_customers`, `achieve_chefs`, `achieve_events`, `achieve_video`) VALUES
-(1, '5', '522', '120', 'https://www.youtube.com/watch?v=Rmcrrf4p_VA&ab_channel=%E0%A6%AE%E0%A6%BE%E0%A6%B0%E0%A6%95%E0%A6%BE%E0%A6%9C%E0%A6%9F%E0%A6%BF%E0%A6%AD%E0%A6%BFMarkazTv');
+(1, '5', '522', '120', 'https://www.youtube.com/watch?v=vJ9lFl3H6SE');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_booking`
+--
+
+CREATE TABLE `tbl_booking` (
+  `booking_id` int(11) NOT NULL,
+  `country_id` int(11) NOT NULL,
+  `city_id` int(11) NOT NULL,
+  `place_id` int(11) NOT NULL,
+  `booking_event_type` varchar(25) NOT NULL,
+  `booking_num_people` varchar(15) NOT NULL,
+  `booking_type_people` varchar(20) NOT NULL,
+  `booking_contact_no` int(15) NOT NULL,
+  `booking_date` varchar(20) NOT NULL,
+  `booking_email` varchar(25) NOT NULL,
+  `booking_status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_city`
+--
+
+CREATE TABLE `tbl_city` (
+  `city_id` int(11) NOT NULL,
+  `city_name` varchar(25) NOT NULL,
+  `city_status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_city`
+--
+
+INSERT INTO `tbl_city` (`city_id`, `city_name`, `city_status`) VALUES
+(1, 'Dhaka', 1),
+(2, 'Rajshahi', 1),
+(3, 'Pabna', 1),
+(4, 'Dubai', 1),
+(5, 'Cox&#039;s Bazar', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_country`
+--
+
+CREATE TABLE `tbl_country` (
+  `country_id` int(11) NOT NULL,
+  `country_name` varchar(25) NOT NULL,
+  `country_status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_country`
+--
+
+INSERT INTO `tbl_country` (`country_id`, `country_name`, `country_status`) VALUES
+(1, 'Bangladesh', 1),
+(2, 'India', 1),
+(3, 'USA', 1),
+(4, 'UK', 1),
+(5, 'UAE', 1);
 
 -- --------------------------------------------------------
 
@@ -109,11 +175,30 @@ CREATE TABLE `tbl_event_category` (
 --
 
 INSERT INTO `tbl_event_category` (`event_cate_id`, `event_cate_name`, `event_cate_slug`) VALUES
-(1, 'All Event', 'all_event'),
-(2, 'Wedding', 'wedding'),
+(2, 'All Events', 'all_events'),
 (3, 'Corporate', 'corporate'),
 (4, 'Cocktail', 'cocktail'),
-(5, 'Buffet', 'buffet');
+(5, 'Buffet', 'buffet'),
+(6, 'Wedding', 'wedding');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_place`
+--
+
+CREATE TABLE `tbl_place` (
+  `place_id` int(11) NOT NULL,
+  `place_name` varchar(25) NOT NULL,
+  `place_status` int(11) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_place`
+--
+
+INSERT INTO `tbl_place` (`place_id`, `place_name`, `place_status`) VALUES
+(1, 'Mirpur Shopping Complex', 1);
 
 -- --------------------------------------------------------
 
@@ -160,7 +245,29 @@ INSERT INTO `tbl_services` (`service_id`, `service_icon`, `service_title`, `serv
 (3, 'fas fa-bold', 'adfafasd', 'fasdfas', 'fasfasf'),
 (4, 'fab fa-app-store-ios', '  this is title  ', 'this is description', 'this is link'),
 (5, 'fas fa-gem', ' this sis service ', 'service description', 'link'),
-(6, 'fab fa-codepen', 'sdfsfgasdgfasdga', 'adgfasgagag', 'adgagag');
+(6, 'fab fa-codepen', 'sdfsfgasdgfasdga', 'adgfasgagag', 'adgagag'),
+(7, 'fab fa-discord', 'new title add', 'this des..............', 'www.hello.com');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tbl_social_galary`
+--
+
+CREATE TABLE `tbl_social_galary` (
+  `socio_galary_id` int(11) NOT NULL,
+  `socio_galary_image` varchar(55) NOT NULL,
+  `socio_galary_status` int(5) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `tbl_social_galary`
+--
+
+INSERT INTO `tbl_social_galary` (`socio_galary_id`, `socio_galary_image`, `socio_galary_status`) VALUES
+(1, 'social_galary_1697950240_43996190.jpg', 1),
+(2, 'social_galary_1697950299_22063342.jpg', 1),
+(3, 'social_galary_1697950311_87483725.png', 1);
 
 -- --------------------------------------------------------
 
@@ -207,7 +314,9 @@ CREATE TABLE `tbl_team` (
 
 INSERT INTO `tbl_team` (`team_id`, `team_name`, `team_designation`, `team_facebook`, `team_twitter`, `team_instagram`, `team_slug`, `team_photo`) VALUES
 (1, 'Ahmad Ali', 'Head Chef', 'https://www.facebook.com/', 'https://twitter.com/i/flow/', 'https://www.instagram.com/', 'U652984c76719e', 'team_1697219783_3203573.jpg'),
-(2, 'Reja Kazi', 'Decoration Chef', 'https://www.facebook.com/', 'twitter.com', 'instagram.com', 'U6529871c0979c', 'team_1697220380_35454765.jpg');
+(2, 'Reja Kazi', 'Decoration Chef', 'https://www.facebook.com/', 'twitter.com', 'instagram.com', 'U6529871c0979c', 'team_1697220380_35454765.jpg'),
+(3, 'Rakib', 'Video Editor', 'fb.com/rakib', 'twiter.com/rakib', 'instagram.com/rakib', 'U65362c08b892f', 'team_1698049032_19778334.jpg'),
+(4, 'Rakib', 'Video Editor', 'fb.com/rakib', 'twiter.com/rakib', 'instagram.com/rakib', 'U6536363726ffe', 'team_1698051639_3715163.jpg');
 
 -- --------------------------------------------------------
 
@@ -257,6 +366,27 @@ ALTER TABLE `tbl_achievement`
   ADD PRIMARY KEY (`achieve_id`);
 
 --
+-- Indexes for table `tbl_booking`
+--
+ALTER TABLE `tbl_booking`
+  ADD PRIMARY KEY (`booking_id`),
+  ADD KEY `country_id` (`country_id`),
+  ADD KEY `place_id` (`place_id`),
+  ADD KEY `city_id` (`city_id`);
+
+--
+-- Indexes for table `tbl_city`
+--
+ALTER TABLE `tbl_city`
+  ADD PRIMARY KEY (`city_id`);
+
+--
+-- Indexes for table `tbl_country`
+--
+ALTER TABLE `tbl_country`
+  ADD PRIMARY KEY (`country_id`);
+
+--
 -- Indexes for table `tbl_event`
 --
 ALTER TABLE `tbl_event`
@@ -270,6 +400,12 @@ ALTER TABLE `tbl_event_category`
   ADD PRIMARY KEY (`event_cate_id`);
 
 --
+-- Indexes for table `tbl_place`
+--
+ALTER TABLE `tbl_place`
+  ADD PRIMARY KEY (`place_id`);
+
+--
 -- Indexes for table `tbl_role`
 --
 ALTER TABLE `tbl_role`
@@ -280,6 +416,12 @@ ALTER TABLE `tbl_role`
 --
 ALTER TABLE `tbl_services`
   ADD PRIMARY KEY (`service_id`);
+
+--
+-- Indexes for table `tbl_social_galary`
+--
+ALTER TABLE `tbl_social_galary`
+  ADD PRIMARY KEY (`socio_galary_id`);
 
 --
 -- Indexes for table `tbl_status`
@@ -318,6 +460,24 @@ ALTER TABLE `tbl_achievement`
   MODIFY `achieve_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
+-- AUTO_INCREMENT for table `tbl_booking`
+--
+ALTER TABLE `tbl_booking`
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `tbl_city`
+--
+ALTER TABLE `tbl_city`
+  MODIFY `city_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `tbl_country`
+--
+ALTER TABLE `tbl_country`
+  MODIFY `country_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT for table `tbl_event`
 --
 ALTER TABLE `tbl_event`
@@ -327,7 +487,13 @@ ALTER TABLE `tbl_event`
 -- AUTO_INCREMENT for table `tbl_event_category`
 --
 ALTER TABLE `tbl_event_category`
-  MODIFY `event_cate_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `event_cate_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `tbl_place`
+--
+ALTER TABLE `tbl_place`
+  MODIFY `place_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `tbl_role`
@@ -339,7 +505,13 @@ ALTER TABLE `tbl_role`
 -- AUTO_INCREMENT for table `tbl_services`
 --
 ALTER TABLE `tbl_services`
-  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `tbl_social_galary`
+--
+ALTER TABLE `tbl_social_galary`
+  MODIFY `socio_galary_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT for table `tbl_status`
@@ -351,7 +523,7 @@ ALTER TABLE `tbl_status`
 -- AUTO_INCREMENT for table `tbl_team`
 --
 ALTER TABLE `tbl_team`
-  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `team_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `tbl_users`
@@ -362,6 +534,14 @@ ALTER TABLE `tbl_users`
 --
 -- Constraints for dumped tables
 --
+
+--
+-- Constraints for table `tbl_booking`
+--
+ALTER TABLE `tbl_booking`
+  ADD CONSTRAINT `tbl_booking_ibfk_1` FOREIGN KEY (`country_id`) REFERENCES `tbl_country` (`country_id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `tbl_booking_ibfk_2` FOREIGN KEY (`place_id`) REFERENCES `tbl_place` (`place_id`),
+  ADD CONSTRAINT `tbl_booking_ibfk_3` FOREIGN KEY (`city_id`) REFERENCES `tbl_city` (`city_id`);
 
 --
 -- Constraints for table `tbl_event`
